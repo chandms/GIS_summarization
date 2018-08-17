327d047bccd71a51202f9ee5db5240f896483b11
Chandrika Mukherjee <chandrika.mjee@gmail.com>
2018-08-18 00:42:34 +0530

    Influx Insertion

--- changedDB.py
+++ changedDB.py
@@ -22,7 +22,6 @@ myclient = InfluxDBClient('127.0.0.1', 8086, 'root', 'root', database='chandrika
 
 myclient.create_database('chandrika')
 myclient.create_retention_policy('awesome_policy', '3d', '3', default=True)
-#myclient = InfluxDBClient(host='127.0.0.1', port=8086)
 
 
 class MySeriesHelper(SeriesHelper):
@@ -102,57 +101,32 @@ def traverse(root):
                             prev = nn
             traverse(node)
 
-def collect(root):
-    if root.childNodes:
-        for node in root.childNodes:
-            if node.nodeType == Node.ELEMENT_NODE:
-                headList.append(node.tagName)
-                if (node.hasAttributes()):
-                    r=list(node.attributes.keys())[0]
-                    f= node.tagName+r
-                    for elem in node.attributes.values():
-                        f=f+elem.firstChild.data
-                    headList.append(f)
-            collect(node)
 
 
-
-kmlfile='C:/Users/Pupul/Desktop/kml/cur1.kml'
+kmlfile='C:/Users/Pupul/Desktop/kml/cur3.kml'
 dom = md.parse(kmlfile)
 root = dom.documentElement
-team = dom.documentElement
-collect(root)
-headSet=set(headList)
-print(headSet)
+
 
 traverse(root)
 
-for key,v in myDict.items():
-    print(key+"-> ")
-    fld.append(key)
-    for l in range(0,len(myDict[key])):
-        print(myDict[key][l])
 
-print("printing field")
+for key,v in myDict.items():
+    fld.append(key)
+
+
 fld.append('long')
 fld.append('lat')
 fld.remove('coordinates')
-print(fld)
+
 for key,val in myDict.items():
     for e in range(0,len(myDict[key])):
         s=myDict[key][e]
         s = re.sub('\s+', '', s)
         if(not s):
             myDict[key][e]=""
-    if("" in myDict[key]):
-        myDict[key].remove("")
 
-
-ll=0
-for key,vl in myDict.items():
-    fd=len(myDict[key])
-    if(fd>ll):
-        ll=fd
+#
 sum=0
 cord= myDict['coordinates']
 nocid=len(cord)
@@ -173,7 +147,21 @@ for j in range(0,len(cord)):
                 myDict['lat'].append(float(st[jk]))
             o=o+1
 
-print("sum = ",sum)
+nml=len(myDict['name'])
+latMap=collections.defaultdict(list)
+longMap=collections.defaultdict(list)
+freq={}
+co=0
+for j in range(nml):
+    lg=myDict['nof'][j]
+    for k in range(lg):
+        latMap[myDict['name'][j]].append(myDict['lat'][k+co])
+        longMap[myDict['name'][j]].append(myDict['long'][k+co])
+        freq[myDict['name'][j]]=lg
+    co=co+lg
+
+
+
 ll=sum
 
 temp = collections.defaultdict(list)
@@ -183,8 +171,10 @@ for key,vg in myDict.items():
         cur_list= f.split('-')
         if(cur_list[1]=='map'):
             temp['map'].append(cur_list[2])
-        else:
+        elif(cur_list[1]=='text'):
             temp['map'].append('')
+        else:
+            temp['map'].append(cur_list[1])
         temp['timeStamp'].append(cur_list[0])
         st=cur_list[3].split('_')
         latlon=cur_list[4].split('_')
@@ -209,7 +199,7 @@ for kk,vk in temp.items():
     for j in range(l):
         myDict[kk].append(temp[kk][j])
 
-fld.append('cid')
+
 fld.append('nof')
 fld.append('map')
 fld.append('posLat')
@@ -247,30 +237,79 @@ for u in range(len(rm)):
 
 
 
-cc=0
-p=0
 
 for t, v in myDict.items():
-    print(t,"->",v,"cm")
+    print(t,"->",v,len(myDict[t]))
 myDict.pop('coordinates', None)
-for j in range(nocid):
-    d= myDict['nof'][j]
 
-    for j in range(d):
-        dc = {}
-        for kk,vv in myDict.items():
-            if(kk!='long' and kk!='lat'):
-                if kk=='Datanametotal':
-                    dc[kk]=myDict[kk][0]
-                elif(kk=='styleUrl' or kk=='name' or kk=='nof' or kk=='cid'):
-                    continue
-                else:
+md = int(myDict['Datanametotal'][0])
+nf=len(myDict['nof'])
+cc=0
+ind=0
+print(len(myDict['long']),len(myDict['lat']))
+jk=0
+d=0
+while jk<nf or cc<md:
+    d=0
+    if(cc<md and myDict['map'][cc]!='' and myDict['map'][cc]!='audio' and myDict['map'][cc]!='video' and myDict['map'][cc]!='image'):
+        d= freq[myDict['map'][cc]]
+    print("d = ", d,"jk = ",jk)
+    g=0
+    if(cc<md):
+        if(myDict['text'][cc]!='' or myDict['map'][cc]=='audio' or myDict['map'][cc]=='video' or myDict['map'][cc]=='image'):
+            g=g+1
+    if(g==1):
+        dc={}
+        for kk,vt in myDict.items():
+            if(kk=='long' or kk=='lat'):
+                dc[kk]=0.000000000000
+            elif(kk=='cid'):
+                continue
+            elif kk=='Datanametotal':
+                dc[kk]=myDict[kk][0]
+            elif(kk=='styleUrl' or kk=='name' or kk=='nof'):
+                continue
+            else:
+                if(myDict[kk][cc]!=''):
                     dc[kk]=myDict[kk][cc]
-            elif(kk=='long'):
-                dc[kk]=myDict[kk][j]
-            elif(kk=='lat'):
-                dc[kk]=myDict[kk][j]
+                else:
+                    dc[kk]="........"
         MySeriesHelper(**dc)
+        for kk, vm in dc.items():
+            print(kk, vm)
+    else:
+        latList=latMap[myDict['map'][cc]]
+        longList=longMap[myDict['map'][cc]]
+        print(latList)
+        print(longList)
+        for j in range(d):
+            print("j = ",j)
+            dc = {}
+            for kk,vt in myDict.items():
+                if(kk!='long' and kk!='lat' and kk!='cid'):
+                    if kk=='Datanametotal':
+                        dc[kk]=myDict[kk][0]
+                    elif(kk=='styleUrl' or kk=='name' or kk=='nof'):
+                        continue
+                    else:
+                        if(myDict[kk][cc]!=''):
+                            dc[kk]=myDict[kk][cc]
+                        else:
+                            dc[kk]="........"
+                elif(kk=='long'):
+                    dc[kk]=longList[j]
+                elif(kk=='lat'):
+                    dc[kk]=latList[j]
+                elif(kk=='cid'):
+                    continue
+            MySeriesHelper(**dc)
+            for kk,vm in dc.items():
+                print(kk,vm)
+
+        jk=jk+1
+
+        ind = ind + d
+    print("ind = ",ind)
     cc=cc+1
 
 
